b00317887c1b30f8fb7311e7db92a04bb25f70c9
60gCreatine <cphah643@gmail.com>
2025-01-26 00:19:15 +0100

    more added shiny pages

--- Soccer.R
+++ Soccer.R
@@ -1,7 +1,11 @@
 library(shiny)
 library(tidyverse)
+library(ggsoccer)
+  # https://github.com/Torvaney/ggsoccer
 # Basic UI og Server
 df <- readRDS("dataforvideo.rds")
+shots <- data.frame(x = c(90, 85, 82, 78, 83, 74, 94, 91),
+                    y = c(43, 40, 52, 56, 44, 71, 60, 54))
 
 library(skimr)
   dfvars=skim(df)
@@ -17,15 +21,31 @@ ui <- fluidPage(
                          choices = c("age","bmi"),
                          selected = "age"
                          ),
-                 # Her kommer plottet
                   plotOutput("spplot")
-             
     ),
     tabPanel("Skud",
+             selectInput("sk",
+                         "Vælg num variabel",
+                         choices = c("myang","mdist"),
+                         selected = "myang"
+                        ),
+                  plotOutput("skplot")
             ),
     tabPanel("Kategoriske var",
+             selectInput("skat",
+                         "Vælg variabel",
+                         choices = c("position","team.name"),
+                         selected = "position"
+                        ),
+                  plotOutput("skatplot")
             ),
     tabPanel("Fodboldbanen",
+             selectInput("player",
+                         "Spillerrelateret variabel",
+                         choices = c("shotpositions","passes"),
+                         selected = "shotpositions"
+             ),
+             plotOutput("soccerplot")
             ),
   )
 )
@@ -35,6 +55,35 @@ server <- function(input, output, session) {
     xval=input$sp
     ggplot(df,aes_string(x=xval))+geom_histogram()
   })
+  output$skplot <- renderPlot({
+    xval=input$sk
+    ggplot(df,aes_string(x=xval))+geom_histogram()
+  })
+  output$skatplot <- renderPlot({
+    xval=input$skat
+    cval="isGoal"
+    ggplot(df,aes_string(x=xval,color=cval))+geom_bar()+
+      #theme(axis.text.x = element_text(angle=90))+
+      coord_flip()
+  })
+  output$soccerplot <- renderPlot({
+    ggplot(shots) +
+      annotate_pitch(colour = "white",
+                     dimensions = pitch_wyscout,
+                     fill   = "springgreen4",
+                     limits = FALSE) +
+      geom_point(aes(x = x, y = y),
+                 colour = "yellow",
+                 size = 4) +
+      theme_pitch() +
+      theme(panel.background = element_rect(fill = "springgreen4")) +
+      coord_flip(xlim = c(49, 101)) +
+      scale_y_reverse() +
+      ggtitle("Simple shotmap",
+              "ggsoccer example")
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
+
