14ea192865be4d75b9d546b3eace7d37afd2e785
60gCreatine <cphah643@gmail.com>
2025-02-01 20:47:25 +0100

    idk

---
+++ Untitled.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    mainPanel(
+      
+    )
+  )
+)
+
+server <- function(input,output,session){
+  
+}
+
+shinyApp(ui, server)
